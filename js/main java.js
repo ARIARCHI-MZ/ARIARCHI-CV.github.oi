d8d11e3e09e543c7275f55ee482ea09c52bd53aa
MARYAM <seyedehmaryam.zahraei@mail.polimi.it>
2023-01-05 22:06:19 +0100

    first eddition

---
+++ js/main java.js
@@ -0,0 +1,36 @@
+"use strict"
+var a=6;
+var b=6;
+var c=a*b;
+console.log(c);
+
+a=13
+c=a*b;
+console.log(c)
+//ES6
+let d=6;
+let e=6;
+let f=a*b;
+console.log(f);
+
+d=8;
+f=a*b;
+console.log(f);
+
+const g="6";
+const h="6";
+const i=a*b;
+console.log(i);
+
+let j=false;
+console.log(j);
+
+var person=["first","last",true];
+console.log(person[1]);
+
+var data={
+    firstN : "M",
+    lastN : "L",
+    job : true
+}
+console.log(data ['firstN']);
